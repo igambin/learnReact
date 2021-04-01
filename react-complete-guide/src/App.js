e2fcede73066626601cf87bb9e96734b864d5d41
Ingo Gambin <ingo.gambin@gmx.de>
2021-04-01 10:08:57 +0200

    use function and useState-hook instead of class and component on App.js

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,36 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    people: [
-      {name: 'Mark', age: 28},
-      {name: 'Maik', age: 27},
-      {name: 'Monk', age: 25}
-    ]
-  }
+const app = props => {
+  const [peopleState, setPeople] = useState(
+    {
+      people: [
+        {name: 'Mark', age: 28},
+        {name: 'Maik', age: 27},
+        {name: 'Monk', age: 25}
+      ],
+      otherData: "otherData"
+   });
 
-  rotatePeopleHandler = () => {
-    let arr = [...this.state.people.slice(1, this.state.people.length), this.state.people[0]];
-    this.setState(
-      {people: arr}
-      );    
+   console.log(peopleState);
+
+   const rotatePeopleHandler = () => {
+    let arr = [...peopleState.people.slice(1, peopleState.people.length), peopleState.people[0]];
+    setPeople({
+      ...peopleState,
+      people: arr
+    });    
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, this is Ingos first React App</h1>
-        <button onClick={this.rotatePeopleHandler}>Rotate People</button>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age}></Person>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}></Person>
-        <Person name={this.state.people[2].name} age={this.state.people[2].age}></Person>
-      </div>
-    );
-    // the jsx above will be compiled to the following line
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, this is Ingos first React App'));
-  }
+  return (
+    <div className="App">
+      <h1>Hi, this is Ingos first React App</h1>
+      <button onClick={rotatePeopleHandler}>Rotate People</button>
+      <Person name={peopleState.people[0].name} age={peopleState.people[0].age}></Person>
+      <Person name={peopleState.people[1].name} age={peopleState.people[1].age}></Person>
+      <Person name={peopleState.people[2].name} age={peopleState.people[2].age}></Person>
+    </div>
+  );
+  // the jsx above will be compiled to the following line
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, this is Ingos first React App'));
 }
 
-export default App;
+export default app;
+
+
+  
+
+
+
