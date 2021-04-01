0b2f387039cc01b84c96a9b4e10985506218146d
Ingo Gambin <ingo.gambin@gmx.de>
2021-04-01 10:16:19 +0200

    Revert "nicer feeling grammar"
    This reverts commit 888f499db5edf0a5c2b839bf48d20ab03ac066ab.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [peopleState, setPeople] = useState(
-    {
-      people: [
-        {name: 'Mark', age: 28},
-        {name: 'Maik', age: 27},
-        {name: 'Monk', age: 25}
-      ],
-      otherData: "otherData"
-   });
-
-   console.log(peopleState);
-
-   const rotatePeopleHandler = () => {
-    let arr = [...peopleState.people.slice(1, peopleState.people.length), peopleState.people[0]];
-    setPeople({
-      ...peopleState,
-      people: arr
-    });    
+class App extends Component {
+  state = {
+    people: [
+      {name: 'Mark', age: 28},
+      {name: 'Maik', age: 27},
+      {name: 'Monk', age: 25}
+    ],
+    otherData: 'otherData'
   };
 
-  return (
-    <div className="App">
-      <h1>Hi, this is Ingos first React App</h1>
-      <button onClick={rotatePeopleHandler}>Rotate People</button>
-      <Person name={peopleState.people[0].name} age={peopleState.people[0].age}></Person>
-      <Person name={peopleState.people[1].name} age={peopleState.people[1].age}></Person>
-      <Person name={peopleState.people[2].name} age={peopleState.people[2].age}></Person>
-    </div>
-  );
-  // the jsx above will be compiled to the following line
-  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, this is Ingos first React App'));
+  switchNameHandler = () => {
+    let arr = [...this.state.people.slice(1, this.state.people.length), this.state.people[0]];
+    this.setState(
+      {
+        people: arr
+      });    
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, this is Ingos first React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}></Person>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}></Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}></Person>
+      </div>
+    );
+    // the jsx above will be compiled to the following line
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, this is Ingos first React App'));
+  }
 }
 
-export default app;
+export default App;
 
 
   
