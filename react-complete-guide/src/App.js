3f96c15aa4c401333e5bbec32be5c77783408320
Ingo Gambin <ingo.gambin@gmx.de>
2021-04-01 11:34:00 +0200

    pass on methods to props and bind values as arguments to them

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,15 +5,20 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     people: [
-      {name: 'Mark', age: 28},
-      {name: 'Maik', age: 27},
-      {name: 'Monk', age: 25}
+      {name: 'Mark', age: 28, clicked: false},
+      {name: 'Maik', age: 27, clicked: false},
+      {name: 'Monk', age: 25, clicked: false}
     ],
     otherData: 'otherData'
   };
 
-  switchNameHandler = () => {
+  rotatePeopleHandler = (clickedRow) => {
     let arr = [...this.state.people.slice(1, this.state.people.length), this.state.people[0]];
+    arr.forEach(p => p.clicked = false);
+    var cP = arr.find(p => p.name === clickedRow);
+    if(clickedRow && cP){
+      cP.clicked = true;
+    }
     this.setState(
       {
         people: arr
@@ -25,10 +30,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, this is Ingos first React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age}></Person>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}></Person>
-        <Person name={this.state.people[2].name} age={this.state.people[2].age}></Person>
+        <button onClick={this.rotatePeopleHandler}>Switch Name</button>
+        <Person click={this.rotatePeopleHandler} name={this.state.people[0].name} age={this.state.people[0].age}></Person>
+        <Person click={this.rotatePeopleHandler} name={this.state.people[1].name} age={this.state.people[1].age}></Person>
+        <Person click={this.rotatePeopleHandler} name={this.state.people[2].name} age={this.state.people[2].age}></Person>
       </div>
     );
     // the jsx above will be compiled to the following line
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,9 +3,10 @@ import React from 'react';
 const person = (props) =>  {
     let name = props.name;
     let age = props.age;
+    let clicked = props.clicked;
     return (
         <div>
-            <p>{name} is {age} years old.</p>
+            <p onClick={props.click.bind(this, name)}>{name} is {age} years old {clicked?"*":""}.</p>
             <p>{props.children}</p>
         </div>
     )
