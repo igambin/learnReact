5afb14b9d7130ea2812e084afc7d2265e2950939
Ingo Gambin <ingo.gambin@gmx.de>
2021-04-06 14:04:16 +0200

    finetune

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,19 +5,19 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     people: [
-      {name: 'Mark', age: 28, clicked: false},
-      {name: 'Maik', age: 27, clicked: false},
-      {name: 'Monk', age: 25, clicked: false}
+      {personId: '1', name: 'Mark', age: 28, clicked: false},
+      {personId: '2', name: 'Maik', age: 27, clicked: false},
+      {personId: '3', name: 'Monk', age: 25, clicked: false}
     ],
     otherData: 'otherData'
   };
 
-  rotatePeopleHandler = (clickedRowName) => {
-    console.log(clickedRowName + ' clicked!');
+  rotatePeopleHandler = (clickedPersonId) => {
+    console.log(clickedPersonId + ' clicked!');
     let arr = [...this.state.people.slice(1, this.state.people.length), this.state.people[0]];
     arr.forEach(p => p.clicked = false);
-    var cP = arr.find(p => p.name === clickedRowName);
-    if(clickedRowName && cP){
+    var cP = arr.find(p => p.personId === clickedPersonId);
+    if(clickedPersonId && cP){
       cP.clicked = true;
     }
     this.setState(
@@ -29,11 +29,11 @@ class App extends Component {
 
   switchNameHandler = (event) => {
     let target = event.target;
-    let name = target.name;
+    let personId = target.name;
     let newname = target.value;
-    console.log("switchNameHandler triggered: " + name + " => " + newname);
-    if(name && newname) {
-      var cP = this.state.people.find(p => p.name === name);
+    console.log("switchNameHandler triggered on Person '" + personId + "' => " + newname);
+    if(personId && newname) {
+      var cP = this.state.people.find(p => p.personId === personId);
       if(cP) {
         cP.name = newname;
         this.setState(
@@ -50,9 +50,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, this is Ingos first React App</h1>
         <button onClick={() => this.rotatePeopleHandler()}>Switch Name</button>
-        <Person click={this.rotatePeopleHandler} changed={this.switchNameHandler} name={this.state.people[0].name} age={this.state.people[0].age} clicked={this.state.people[0].clicked}></Person>
-        <Person click={this.rotatePeopleHandler} changed={this.switchNameHandler} name={this.state.people[1].name} age={this.state.people[1].age} clicked={this.state.people[1].clicked}></Person>
-        <Person click={this.rotatePeopleHandler} changed={this.switchNameHandler} name={this.state.people[2].name} age={this.state.people[2].age} clicked={this.state.people[2].clicked}></Person>
+        <Person click={this.rotatePeopleHandler} changed={this.switchNameHandler} personId={this.state.people[0].personId} name={this.state.people[0].name} age={this.state.people[0].age} clicked={this.state.people[0].clicked}></Person>
+        <Person click={this.rotatePeopleHandler} changed={this.switchNameHandler} personId={this.state.people[1].personId} name={this.state.people[1].name} age={this.state.people[1].age} clicked={this.state.people[1].clicked}></Person>
+        <Person click={this.rotatePeopleHandler} changed={this.switchNameHandler} personId={this.state.people[2].personId} name={this.state.people[2].name} age={this.state.people[2].age} clicked={this.state.people[2].clicked}></Person>
       </div>
     );
     // the jsx above will be compiled to the following line
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Person.css'
 const person = (props) =>  {
+    let personId = props.personId;
     let name = props.name;
     let age = props.age;
     let clicked = props.clicked;
     return (
         <div className="Person">
-            <p><input type="text" name={name} onChange={props.changed.bind(this)} value={name} /><span onClick={props.click.bind(this, name)}> is {age} years old {clicked?"*":""}</span></p>
+            <p><input type="text" name={personId} onChange={props.changed.bind(this)} value={name} /><span onClick={props.click.bind(this, personId)}> is {age} years old {clicked?"*":""}</span></p>
         </div>
     )
 };
