ebc069091cfc81ef91bb9c5a987da8c1f988c736
Ingo Gambin <ingo.gambin@gmx.de>
2021-05-24 12:17:09 +0200

    using styled-components

--- 02_styling_react_components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 02_styling_react_components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,10 +1,43 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => props.invalid ? 'red' : 'navy'};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+    background: ${props => props.invalid ? 'mistyrose' : 'transparent'};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: AliceBlue;
+    border-color: navy;
+  }
+`;
+
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState('true');
+
+  const resetValidation = () => {
+    setIsValid(true);
+  };
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
@@ -12,15 +45,27 @@ const CourseInput = props => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    
+    resetValidation();
+
+    if(enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
+    setEnteredValue('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+        <input 
+          type="text"
+          onChange={goalInputChangeHandler}
+          onKeyPress={resetValidation}
+          value={enteredValue} />
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- 02_styling_react_components/src/components/UI/Button/Button.js
+++ 02_styling_react_components/src/components/UI/Button/Button.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import styled from 'styled-components';
 
 import './Button.css';
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+const Button = styled.button`
+    width: 100%;
+    font: inherit;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid navy;
+    color: white;
+    background: navy;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+    cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: MediumBlue;
+    border-color: MidnightBlue;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+  `;
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
