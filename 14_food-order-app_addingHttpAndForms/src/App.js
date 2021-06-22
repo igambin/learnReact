75c705208e401f51e08c961899a5f7ac98233644
Ingo Gambin <ingo.gambin@googlemail.com>
2021-06-22 11:51:52 +0200

    init next take on food order app

---
+++ 14_food-order-app_addingHttpAndForms/src/App.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useState } from 'react';
+import Cart from './Components/Cart/Cart';
+import Header from "./Components/Layout/Header";
+import Meals from './Components/Meals/Meals';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
+  return (
+    <Fragment>
+      {cartIsShown && <Cart onHideCart={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ 14_food-order-app_addingHttpAndForms/src/store/Util.js
@@ -0,0 +1,7 @@
+export const EuroFormatter = new Intl.NumberFormat(
+  'de-DE', 
+  {
+    style: 'currency',
+    currency: 'EUR'
+  }
+);
