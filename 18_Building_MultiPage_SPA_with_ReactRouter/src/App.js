48d55076ced33b033c7a6fdcbcc51645d49e5040
Ingo Gambin <ingo.gambin@googlemail.com>
2021-06-25 08:49:56 +0200

    first steps and basics about creating routing with Route, Switch, Redirect, Link, NavLink

--- 18_Building_MultiPage_SPA_with_ReactRouter/src/App.js
+++ 18_Building_MultiPage_SPA_with_ReactRouter/src/App.js
@@ -1,7 +1,29 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./components/pages/Products/ProductDetail";
+import Products from "./components/pages/Products/Products";
+import Welcome from "./components/pages/Welcome/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:id">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
