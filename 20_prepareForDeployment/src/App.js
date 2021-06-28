37edbbb4240459a1b0ab51ba5fe970992082ede2
Ingo Gambin <ingo.gambin@googlemail.com>
2021-06-28 10:41:52 +0200

    add lazy loading to tell react to split the js code into chunks which are loaded only when required

--- 20_prepareForDeployment/src/App.js
+++ 20_prepareForDeployment/src/App.js
@@ -1,30 +1,41 @@
+import React, { Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import QuoteDetail from "./pages/QuoteDetail";
-import QuoteNew from "./pages/QuoteNew";
 import QuoteIndex from "./pages/QuoteIndex";
-import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const QuoteNew = React.lazy(() => import("./pages/QuoteNew"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <QuoteIndex />
-        </Route>
-        <Route path="/quotes/new" exact>
-          <QuoteNew />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <QuoteIndex />
+          </Route>
+          <Route path="/quotes/new" exact>
+            <QuoteNew />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
